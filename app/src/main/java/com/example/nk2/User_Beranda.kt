7fdbba6212f1f0f2539c2ab99da6030dd14fa2d3
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2020-12-16 12:25:36 +0700

    Update Recycler View

--- app/src/main/java/com/example/nk2/User_Beranda.kt
+++ app/src/main/java/com/example/nk2/User_Beranda.kt
@@ -1,52 +1,136 @@
 package com.example.nk2
 
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.nk2.model.Mahasiswa
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nk2.adapter.BerandaAdapter
 import com.example.nk2.model.Toko
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.protobuf.Empty
+import kotlinx.android.synthetic.main.user_beranda.*
+import kotlinx.coroutines.tasks.await
 
-class User_Beranda: AppCompatActivity() {
+class User_Beranda : AppCompatActivity() {
 
     val fStore = FirebaseFirestore.getInstance()
-    var Data : MutableList<Mahasiswa> = arrayListOf()
+    var Data: MutableList<Toko> = arrayListOf()
     var idToko = ""
     var desk = ""
     var nama = ""
     var menu = ""
     var telp = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_beranda)
-        getAllToko()
+
+        rc_card.layoutManager = LinearLayoutManager(this)
+
+        val df = fStore.collection("Toko").get().addOnSuccessListener {
+            var tokoItem: Toko
+            for(document in it) {
+                val name = document.data["name"].toString()
+                idToko = document.id
+                nama = document.data["NamaToko"].toString()
+                desk = document.data["Deskripsi"].toString()
+                telp = document.data["Telp"].toString()
+                menu = document.data["Menu"].toString()
+                tokoItem = Toko(idToko, nama, desk, telp, menu)
+                Data.add(tokoItem)
+//                Log.d(
+//                    android.content.ContentValues.TAG,
+//                    "${document.id} => ${Data}"
+//                )
+            }
+            val adapter = BerandaAdapter(Data, this, this)
+            adapter.notifyDataSetChanged()
+            //tampilkan data dalam recycler view
+            rc_card!!.adapter = adapter
+            Log.d("nilainya", Data.size.toString())
+        }
+//        callbackToko : MyCallback
+//        getToko(object: MyCallback {
+//            override fun onCallback(value: MutableList<Toko>) {
+//                Log.d("nilainya", Data.size.toString())
+//            }
+//        })
     }
 
-    fun getAllToko(){
-        Data.clear()
-        var tokoItem : Toko
-        val df = fStore.collection("Toko").get().addOnSuccessListener { result ->
-            for (document in result) {
-                idToko = document.id
-                nama = document.data.get("NamaToko").toString()
-                desk = document.data.get("Deskripsi").toString()
-                telp = document.data.get("Telp").toString()
-                menu = document.data.get("Menu").toString()
-                tokoItem = Toko(idToko, nama, desk, telp, menu)
-                Log.d(ContentValues.TAG, "${document.id} => ${tokoItem.toString()}")
-            }
-        }.addOnFailureListener { exception ->
-                Log.d(ContentValues.TAG, "Error getting documents: ", exception)
-            }
-        Log.d("Apaini = ", Data.toString())
-    }
     fun beranda_profil(view: View) {
-        val intent = Intent (applicationContext, User_Profil::class.java)
+        val intent = Intent(applicationContext, User_Profil::class.java)
         startActivity(intent)
     }
+
+//    override fun onDataChange(){
+//
+//    }
+//
+//    suspend fun getTokos() {
+//        var Data = getAllTokoFirestore().toObject(Toko::class.java)
+//    }
+
+//    suspend fun getAllTokoFirestore(): DocumentSnapshot {
+//        return try {
+//            val data = fStore
+//                .collection("users")
+//                .document("Toko")
+//                .get()
+//                .await()
+//            data
+//        } catch (e: Exception) {
+//            Log.d("Galat", "Galat Mengambil Data")
+//            null!!
+//        }
+////        df. .addOnCompleteListener {
+////            if (it.isSuccessful) {
+////                var tokoItem : Toko
+////                Data.clear()
+////                for (document in it.result!!) {
+////                    idToko = document.id
+////                    nama = document.data["NamaToko"].toString()
+////                    desk = document.data["Deskripsi"].toString()
+////                    telp = document.data["Telp"].toString()
+////                    menu = document.data["Menu"].toString()
+////                    tokoItem = com.example.nk2.model.Toko(idToko, nama, desk, telp, menu)
+////                    Data.add(tokoItem)
+////                    android.util.Log.d(
+////                        android.content.ContentValues.TAG,
+////                        "${document.id} => ${Data}"
+////                    )
+////                }
+////            }
+//    }
+//}
+
+
+//    fun getToko(myCallback: MyCallback) {
+//        fStore.collection("Toko").get().addOnCompleteListener { task ->
+//            if (task.isSuccessful) {
+//                val list = mutableListOf<Toko>()
+//                var tokoItem: Toko
+//                Data.clear()
+//
+////                val types: List<Toko> = documentSnapshots.toObjects(Toko::class.java)
+//                for (document in task.result!!) {
+//                    val name = document.data["name"].toString()
+//                    idToko = document.id
+//                    nama = document.data["NamaToko"].toString()
+//                    desk = document.data["Deskripsi"].toString()
+//                    telp = document.data["Telp"].toString()
+//                    menu = document.data["Menu"].toString()
+//                    tokoItem = com.example.nk2.model.Toko(idToko, nama, desk, telp, menu)
+//                    list.add(tokoItem)
+//
+//                }
+//                myCallback.onCallback(list)
+//            }
+//        }
+//    }
+//    interface MyCallback {
+//        fun onCallback(value: MutableList<Toko>)
+//    }
 }
--- app/src/main/java/com/example/nk2/adapter/BerandaAdapter.kt
+++ app/src/main/java/com/example/nk2/adapter/BerandaAdapter.kt
@@ -1,4 +1,64 @@
 package com.example.nk2.adapter
 
-class BerandaAdapter {
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nk2.CustomOnItemClickListener
+import com.example.nk2.R
+import com.example.nk2.User_Profil
+import com.example.nk2.model.Toko
+import kotlinx.android.synthetic.main.user_beranda_toko_item.view.*
+
+class BerandaAdapter(private val Tokos:MutableList<Toko>, val context: Context, val activity: Activity) : RecyclerView.Adapter<BerandaAdapter.Holder>(){
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(toko: Toko) {
+            with(itemView){
+                tv_toko_nama.text = toko.NamaToko
+                tv_toko_desc.text = toko.Deskripsi
+                Log.d("nilainyahhh", Tokos.size.toString())
+                cardList.setOnClickListener(CustomOnItemClickListener(adapterPosition, object : CustomOnItemClickListener.OnItemClickCallback {
+                    override fun onItemClicked(view: View, position: Int) {
+                        val item = Tokos.get(position)
+                        var itemName = item.NamaToko
+                        Log.d("***", "${position}");
+                        val intent = Intent(context, User_Profil::class.java)
+                        intent.putExtra("ID", item.id)
+                        intent.putExtra("NAMA", item.NamaToko)
+                        intent.putExtra("DESC", item.Deskripsi)
+                        intent.putExtra("MENU", item.Menu)
+                        context.startActivity(intent)
+                    }
+                }))
+//                lbList.setOnClickListener(CustomOnItemClickListener(adapterPosition, object : CustomOnItemClickListener.OnItemClickCallback {
+//                    override fun onItemClicked(view: View, position: Int) {
+//                        val item = list.get(position)
+//                        var itemName = item.name
+//                        var itemJenis = item.jenis
+//                        Log.d("***", "${position}");
+//                        val intent = Intent(context, Coba::class.java)
+//                        intent.putExtra("idUser", position)
+//                        intent.putExtra("namaUser", itemName)
+//                        intent.putExtra("jenisUser", itemJenis)
+//                        context.startActivity(intent)
+//                    }
+//                }))
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BerandaAdapter.Holder {
+        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.user_beranda_toko_item,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: BerandaAdapter.Holder, position: Int) {
+        holder.bind(Tokos[position])
+    }
+
+    override fun getItemCount(): Int = Tokos?.size
+
+
 }
